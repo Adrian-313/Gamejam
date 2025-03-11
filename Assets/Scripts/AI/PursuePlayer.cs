f80f3e11462f6296529dd57be0018014c790435b
Lyzder <santiagot791@gmail.com>
2025-03-11 16:29:41 -0500

    Main map navmesh
    1. Se borró partes innecesarias del map
    2. Se agrupó los elementos que componen el piso
    3. Se implementó un navmesh
    4. Se agregó el enemigo a la escena
    5. Se implementaron animaciones para el enemigo

--- Assets/Scripts/AI/PursuePlayer.cs
+++ Assets/Scripts/AI/PursuePlayer.cs
@@ -5,19 +5,61 @@ public class PursuePlayer : MonoBehaviour
 {
     public Camera cam;
     public NavMeshAgent agent;
+    public Animator animator;
+    public float attackRadius;
+
+    [SerializeField] Transform hitbox;
+    [SerializeField] LayerMask attackLayer;
+
+    private Transform player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+        // For the lack of player still, this is for mere testing purposes
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        //    RaycastHit hit;
 
-            if(Physics.Raycast(ray, out hit))
-            {
-                agent.SetDestination(hit.point);
-            }
+        //    if(Physics.Raycast(ray, out hit))
+        //    {
+        //        agent.SetDestination(hit.point);
+        //    }
+        //}
+        //---
+
+        ChasePlayer();
+        TransitionAnimation();
+        Attack();
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void TransitionAnimation()
+    {
+        bool isMoving = agent.velocity.magnitude > 0.1f && agent.remainingDistance > agent.stoppingDistance;
+        // Set animation parameter
+        animator.SetBool("isRunning", isMoving);
+    }
+
+    private void Attack()
+    {
+        Collider[] objetos = Physics.OverlapSphere(hitbox.position, attackRadius, attackLayer);
+
+        if (objetos.Length > 0)
+        {
+            animator.ResetTrigger("attack");
+            animator.SetTrigger("attack");
+            // TODO: Player attacked
         }
     }
 }
